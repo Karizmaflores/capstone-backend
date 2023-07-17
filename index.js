39ae4da1441a5ea8f6a2df5c5fe23bfaf3251e3a
Karizmaflores <flores_karizma@yahoo.com>
2023-07-17 09:40:49 -0500

    untangled mess, auth-101 had capstone code

--- index.js
+++ index.js
@@ -1,90 +1,26 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const { movies, users } = require("./mockData");
+
 const app = express();
+
+//routes
+const usersRouter = require("./src/routes/users");
+const signupRouter = require("./src/routes/signup");
+const signinRouter = require("./src/routes/signin");
+
+//middleware
+const authenticateJWT = require("./src/auth");
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use("/", usersRouter);
+app.use("/", signupRouter);
+app.use("/", signinRouter);
 
-function authenticateJWT(req, res, next){
-    //get authentification header
-    const authHeader = req.headers.authorization;
-    // console.log(authHeader);
-
-    //check if bearer token exists
-    if(authHeader){
-        const token = authHeader.split(" ")[1];
-
-        jwt.verify(token, "tacosaregood", (err, user) => {
-            if (err){
-                res.sendStatus(403);
-            }
-            if(user.role === 'USER'){
-                req.user = user;
-            }
-            
-            next();
-        });
-    } else{
-        res.sendStatus(403);
-    }
-
-}
-
-app.get("/movies", authenticateJWT, (req, res) => {
-    console.log(req);
-    res.json(movies);
-});
-
-app.get("/movies/:id", (req, res) => {
-    const { id } = req.params;
-    const foundMovie = movies.find((movie) => movies.id === +id);
-    res.json(foundMovie);
-});
-
-app.get("/users", (req, res) => {
-    res.json(users);
-});
-
-app.post("/signin", async (req, res) => {
-    const { email, password } = req.body;
-    const foundUser = users.find((user) => user.email === email);
-
-    const hashedPassword = await bcrypt.compare(password, foundUser.password);
-
-   
-     console.log({hashedPassword})
-
-    if(hashedPassword){
-
-        const token = jwt.sign(foundUser, "tacosaregood");
-
-        res.json({
-            token,
-        });
-    } else{
-        res.json({
-            message: 'Email or password is incorrect!',
-        });
-    }  
-});
-
-app.post("/signup", async (req, res) => {
-    const { email, password } = req.body;
-
-    const hashedPassword = await bcrypt.hash(password, 8);
-    // console.log(password);
-    // console.log(hashedPassword);
-
-    users.push({
-        email,
-        password: hashedPassword,
-        role: 'USER'
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to the API",
     });
-
-    res.json(users.at(-1));
 });
 
 app.listen(PORT, console.log(`Listening on ${PORT}`));
---
+++ src/controllers/signup.js
@@ -0,0 +1,31 @@
+const bcrypt = require("bcrypt");
+const pool = require("../sql/connection");
+
+const signup = async (req, res) => {
+
+const { email, password, first_name } = req.body;
+
+const hashedPassword = await bcrypt.hash(password, 8);
+// console.log(password);
+// console.log(hashedPassword);
+
+pool.query(`INSERT INTO ?? (??, ??, ??) VALUES (?, ?, ?)`,
+ ["users","email", "password", "first_name",
+  email, hashedPassword, first_name], 
+ (err, rows, fields) => {
+    res.json({
+        message: "User created!"
+    });
+});
+
+// users.push({
+//     email,
+//     password: hashedPassword,
+// });
+
+res.json(users.at(-1));
+};
+
+module.exports = {
+    signup,
+};
